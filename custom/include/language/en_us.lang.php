5716f871e330887d28ff5ad064f4f417cd885bf1
dev <contact@urdhva-tech.com>
2026-05-12 15:34:51 +0530

    -- DocuSign Template dropdown.
    -- DocuSign Template dropdown.

---
+++ custom/include/language/en_us.lang.php
@@ -0,0 +1,13 @@
+<?php
+$app_list_strings['docusign_templates_list']=array (
+  'ded99898-4123-460c-86b5-020823aad921' => 'API Req Builder: NDA 2',
+  '58ddd6fb-4e74-4652-b519-4b7e5a0a1559' => 'Application for a Medicare provider number',
+  '8ace6741-6162-46cf-83e8-97ba9c171b97' => 'Application for medical practitioner',
+  'd1b75ddf-379b-4cbb-b4c4-5707e6843304' => 'Hasmukh 18Spe2025',
+  '4b3740de-f232-42b3-8cb3-5f60113fd27e' => 'Online Claiming Provider Agreement',
+  '6b74fb06-7e2c-45c2-b873-53e7d80d8f5a' => 'PDF- Quotation - Hardcover + jacket',
+  'd3b454f6-f464-4740-8ae6-a7e514b33b8b' => 'Quotation - Hardcover + Jacket',
+  '156ec576-1ea4-4ec3-832e-3acdd84c054b' => 'Signed And CC',
+  '73cfa567-58a4-4725-bb22-ae8a83b446a6' => 'With Single Signer',
+  '878b556c-95fb-4a98-85c4-a39c5747b9b9' => 'Without Signer',
+);
